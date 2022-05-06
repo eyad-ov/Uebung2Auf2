423a5d6ca90fc1fb39b5f4c37c39bcc53f0e6690
eyad.almasri <eyad.almasri@th-bingen.de>
2022-05-06 19:47:21 +0200

    maybe final solution

--- src/RailAB.java
+++ src/RailAB.java
@@ -1,31 +1,42 @@
+import java.util.ArrayList;
+
 public class RailAB {
     // first time all west waiting!?
-    int trains = 3;
+    //int trains = 3;
+    ArrayList<Train> trains = new ArrayList<Train>();
     boolean direction = true; //east when true
 
     public synchronized void enter(Train train){
 
-        while(trains == 0 || this.direction != train.getDirection()){
+        while(trains.size()==3 || this.direction != train.getDirection()){
                 try{
                     wait();
                 }
 
                 catch(InterruptedException ex){}
 
-                if(trains == 3){
+                if(trains.isEmpty()){
                     this.direction = train.getDirection();
                 }
             }
 
         notifyAll(); // wichtig !?
-        trains--;
+        trains.add(train);
         System.out.println("train " + train.getTrainID() + " entered the track section, the direction is " +(direction ?"east":"west"));
 
     }
 
     public synchronized void leave(Train train){
         // rheinfolge achten
-        trains++;
+        while(trains.get(0)!= train){
+            try{
+                wait();
+            }
+            catch(InterruptedException ex){
+                ex.printStackTrace();
+            }
+        }
+        trains.remove(train);
         System.out.println("train " + train.getTrainID() + " left the track section");
         notifyAll();
     }
